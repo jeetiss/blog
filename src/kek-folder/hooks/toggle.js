e5ced391eadc557ee07fd0cdd7431ef551038058
jeetiss <za5rubley@gmail.com>
2018-12-14 21:09:21 +0300

    delete unused files

--- src/kek-folder/hooks/toggle.js
+++
@@ -1,8 +0,0 @@
-import React, {useState, useCallback} from 'react'
-
-export default (initial) => {
-  const [value, setValue] = useState(initial)
-  const toggle = useCallback(() => setValue(!value))
-
-  return [value, toggle]
-}
