826a7f2b44658e76ce205be66df8408d738315e0
jeetiss <za5rubley@gmail.com>
2019-01-10 15:47:16 +0300

    fix content blink

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,12 +3,13 @@ import styled from "styled-components";
 
 const Inner = styled.video`
   width: 100%;
+  height: 100%;
   display: block;
 
   border-radius: 16px;
   overflow: hidden;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 16px 32px;
-  border: 1px solid rgba(0,0,0,.10);
+  border: 1px solid rgba(0, 0, 0, 0.1);
 
   transition: box-shadow 0.3s ease;
 
@@ -17,8 +18,31 @@ const Inner = styled.video`
   }
 `;
 
+const Relative = styled.div`
+  position: relative;
+`;
+
+const Absolute = styled.div`
+  position: absolute;
+  
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`;
+
+const RatioBlock = ({ ratio, children }) => (
+  <Relative>
+    <div style={{ paddingBottom: Math.round(100 / ratio) + "%" }} />
+
+    <Absolute>{children}</Absolute>
+  </Relative>
+);
+
 const Video = ({ src }) => (
-  <Inner src={src} autoPlay loop muted webkitplaysinline playsInline />
+  <RatioBlock ratio={16 / 9}>
+    <Inner src={src} autoPlay loop muted webkitplaysinline playsInline />
+  </RatioBlock>
 );
 
 export default Video;
