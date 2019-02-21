565e16059d07d03b75039e774d9e3b62f05bcb7b
jeetiss <za5rubley@gmail.com>
2019-02-21 13:44:09 +0300

    wip-slider

--- src/pages/slider.js
+++ src/pages/slider.js
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useReducer
+} from "react";
 import styled from "styled-components";
 
 const noop = () => void 0;
@@ -22,29 +28,6 @@ function removeListener(element, eventNames, listener) {
     element.removeEventListener(events[i], listener, false);
   }
 }
-
-// const useListeners = () => {
-//   const listeners = useRef([]);
-
-//   const addListener = useCallback((element, eventNames, listener) =>
-//     listeners.current.push(_addListener(element, eventNames, listener))
-//   );
-
-//   const removeListener = useCallback((element, eventNames, listener) =>
-//     _removeListener(element, eventNames, listener)
-//   );
-
-//   const removeAll = useCallback(() => {
-//     listeners.current.forEach(unsub => unsub());
-//     listeners.current = [];
-//   });
-
-//   return {
-//     addListener,
-//     removeListener,
-//     removeAll
-//   };
-// };
 
 const defaultCallbacks = { onDrag: noop, onDragStart: noop, onDragEnd: noop };
 
@@ -114,34 +97,176 @@ const Squre = styled.div`
   background-color: wheat;
 `;
 
-const Draggable = () => {
-  const draggableRef = useRef();
-  const [draggingPosition, setDraggingPosition] = useState(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+const useBoundingClientRect = (nodeRef, onChange, { resize, scroll }) => {
+  const [rect, setRect] = useState({});
 
-  useDraggable(draggableRef, {
-    onDragStart: pos => console.log("start", pos) || setDraggingPosition(pos),
-    onDrag: ({ x, y }) =>
-      console.log("progress") ||
-      (draggingPosition &&
-        setPosition({
-          x: x - draggingPosition.x,
-          y: y - draggingPosition.y
-        })),
-    onDragEnd: e => console.log("stop", e) || setDraggingPosition(null)
+  const handleChange = () =>
+    nodeRef.current && onChange(nodeRef.current.getBoundingClientRect());
+
+  useEffect(handleChange, [nodeRef.current]);
+
+  useEffect(() => {
+    resize && addListener(window, "resize", handleChange);
+    scroll && addListener(document, "scroll", handleChange);
+
+    return () => {
+      removeListener(window, "resize", handleChange);
+      removeListener(document, "scroll", handleChange);
+    };
+  }, [resize, scroll]);
+
+  return [rect, setRect];
+};
+
+const initialState = {
+  value: 50,
+  persent: 50,
+
+  left: null,
+  top: null,
+  width: null,
+  height: null,
+
+  isDragging: false,
+  position: { x: null, y: null },
+  dragPosition: { x: null, y: null },
+
+  min: 0,
+  max: 100
+};
+
+const minmax = (min, max) => value => Math.min(Math.max(min, value), max);
+const borders = minmax(0, 1);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DRAG_START": {
+      return { ...state, isDragging: true, dragPosition: action.position };
+    }
+    case "DRAGINNG": {
+      const { position } = action;
+      const persent = borders((position.x - state.left) / state.width);
+      const value = (state.max - state.min) * persent + state.min;
+
+      return { ...state, value, persent: persent * 100, position };
+    }
+    case "DRAG_END": {
+      return { ...state, isDragging: false, dragPosition: null };
+    }
+
+    case "RESIZE": {
+      return {
+        ...state,
+        left: action.size.left,
+        top: action.size.top,
+        width: action.size.width,
+        height: action.size.height
+      };
+    }
+    default: {
+      throw new Error(`unknown action ${action.type}`);
+    }
+  }
+};
+
+const useSlider = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const trackRef = useRef();
+  const thumbRef = useRef();
+
+  useDraggable(trackRef, {
+    onDragStart: position => dispatch({ type: "DRAG_START", position }),
+    onDrag: position => dispatch({ type: "DRAGINNG", position }),
+    onDragEnd: () => dispatch({ type: "DRAG_END" })
   });
 
+  useBoundingClientRect(
+    trackRef,
+    trackRect => dispatch({ type: "RESIZE", size: trackRect }),
+    { resize: true }
+  );
+
+  return [trackRef, thumbRef, state];
+};
+
+const useLogger = state => {
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+};
+
+const Slider = () => {
+  const [trackRef, thumbRef, state] = useSlider();
+
+  useLogger(state);
+
   return (
-    <Squre
-      ref={draggableRef}
+    <div
       style={{
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        cursor: draggingPosition ? "grabbing" : "grab"
+        position: "relative",
+        width: "300px",
+        height: "32px"
       }}
-    />
+      ref={trackRef}
+    >
+      <div
+        style={{
+          userSelect: "none",
+          position: "absolute",
+          top: "12px",
+          width: "100%",
+          height: "8px",
+          backgroundColor: "#E2e2e2"
+        }}
+      />
+
+      <div
+        style={{ width: "100%", transform: `translateX(${state.persent}%)` }}
+      >
+        <div
+          style={{
+            width: "32px",
+            height: "32px",
+            borderRadius: "50%",
+            backgroundColor: "black",
+            transform: "translateX(-50%)"
+          }}
+          ref={thumbRef}
+        />
+      </div>
+    </div>
   );
 };
 
 export default () => {
-  return <Draggable />;
+  return <Slider />;
 };
+
+// const Draggable = () => {
+//   const draggableRef = useRef();
+//   const [draggingPosition, setDraggingPosition] = useState(null);
+//   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+//   useDraggable(draggableRef, {
+//     onDragStart: pos => console.log("start", pos) || setDraggingPosition(pos),
+//     onDrag: ({ x, y }) =>
+//       console.log("progress") ||
+//       (draggingPosition &&
+//         setPosition({
+//           x: x - draggingPosition.x,
+//           y: y - draggingPosition.y
+//         })),
+//     onDragEnd: e => console.log("stop", e) || setDraggingPosition(null)
+//   });
+
+//   return (
+//     <Squre
+//       ref={draggableRef}
+//       style={{
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         cursor: draggingPosition ? "grabbing" : "grab"
+//       }}
+//     />
+//   );
+// };
